3b959361509abfa19a520d48d5bff047e9a002e4
Richard Muhvic <richardmuhvic@gmail.com>
2021-11-29 15:09:33 +0100

    ajout des éléments pour le banner et sickyBanner dasn el fichier JS, création du On Scroll

--- main.js
+++ main.js
@@ -2,13 +2,25 @@
 window.onscroll = function() {myFunction()};
 
 var header = document.getElementById("header");
+var banner = document.getElementById("banner");
 
 var sticky = header.offsetTop;
+var stickyBanner = banner.offsetTop;
 
 function myFunction() {
-  if (window.pageYOffset > sticky) {
-    header.classList.add("sticky");
-  } else {
-    header.classList.remove("sticky");
+    if (window.pageYOffset > sticky) {
+        header.classList.add("sticky");
+    } 
+    else {
+        header.classList.remove("sticky");
   }
 }
+
+function myFunction() {
+    if (window.pageYOffset > sticky) {
+        banner.classList.add("stickyBanner");
+    } 
+    else {
+        banner.classList.remove("stickyBanner");
+    }
+  }
