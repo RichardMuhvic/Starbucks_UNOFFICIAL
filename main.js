5d78271b63c71da9723197c2043b522f6c1c4fd3
Richard Muhvic <richardmuhvic@gmail.com>
2021-11-29 15:13:34 +0100

    changement de lafonction dans JS pour On Scroll

--- main.js
+++ main.js
@@ -1,26 +1,21 @@
-//On Scroll
+//On Scroll Header and Banner
 window.onscroll = function() {myFunction()};
 
 var header = document.getElementById("header");
 var banner = document.getElementById("banner");
 
+
 var sticky = header.offsetTop;
 var stickyBanner = banner.offsetTop;
+
 
 function myFunction() {
     if (window.pageYOffset > sticky) {
         header.classList.add("sticky");
-    } 
-    else {
-        header.classList.remove("sticky");
-  }
-}
-
-function myFunction() {
-    if (window.pageYOffset > sticky) {
         banner.classList.add("stickyBanner");
     } 
     else {
+        header.classList.remove("sticky");
         banner.classList.remove("stickyBanner");
-    }
   }
+}
