48714b6b1ee4f921e48c0182752e290715f27eda
Richard Muhvic <richardmuhvic@gmail.com>
2021-12-01 21:29:31 +0100

    création de l'animation du bouton

--- main.js
+++ main.js
@@ -30,13 +30,11 @@ function openNumber(evt, blockNumber) {
       tabcontent[i].style.display = "none";
     }
   
-    // Get all elements with class="tablinks" and remove the class "active"
     tablinks = document.getElementsByClassName("tablinks");
     for (i = 0; i < tablinks.length; i++) {
       tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
   
-    // Show the current tab, and add an "active" class to the button that opened the tab
     document.getElementById(blockNumber).style.display = "block";
     evt.currentTarget.className += " active";
 }
